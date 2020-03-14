76ffd27c05f2b2ca365b06544a868bebcb1b6288
B Ricks <bricks@unomaha.edu>
2020-03-14 15:52:31 -0500

    Comment Point class. #6

--- src/engine/base/Point.js
+++ src/engine/base/Point.js
@@ -12,15 +12,37 @@ class Point {
         this.x = x;
         this.y = y;
     }
+
+    /**
+     * Calculate the Euclidian distance between this point and another point.
+     * 
+     * @param {Point} otherPoint The point to which we are calculating a distance
+     */
     distance(otherPoint) {
 
         return Math.sqrt(this.distanceSquared(otherPoint));
     }
+
+    /**
+     * Since finding the Euclidean distance requires an expensive square root
+     * calculation, we have the option of calculating the squared distance to go
+     * quicker.
+     * 
+     * @param {Point} otherPoint The point to which we are calculating the
+     * squared distance
+     */
     distanceSquared(otherPoint) {
         let xDiff = (this.x - otherPoint.x);
         let yDiff = (this.y - otherPoint.y);
         return xDiff * xDiff + yDiff * yDiff;
     }
+
+    /**
+     * Find the pairwise difference to another point.
+     * 
+     * @param {Point} otherPoint The point from which we are doing a pairwise
+     * difference. 
+     */
     diff(otherPoint) {
         return new Point(this.x - otherPoint.x, this.y - otherPoint.y);
     }
