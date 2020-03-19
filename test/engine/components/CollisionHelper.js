c394fbb426311bc58c6a78a01567af4f1a7df947
B. Ricks <bricks@unomaha.edu>
2020-03-19 16:30:09 -0500

    Stub out triangles and convex. #9

--- test/engine/components/CollisionHelper.js
+++ test/engine/components/CollisionHelper.js
@@ -4,6 +4,8 @@ import AABBCollider from "../../../src/Engine/components/AABBCollider.js";
 import CollisionHelper from "../../../src/Engine/components/CollisionHelper.js";
 import CircleCollider from "../../../src/Engine/components/CircleCollider.js";
 import PointCollider from "../../../src/Engine/components/PointCollider.js";
+import TriangleCollider from "../../../src/Engine/components/TriangleCollider.js";
+import ConvexCollider from "../../../src/Engine/components/ConvexCollider.js";
 
 let assert = chai.assert;
 let expect = chai.expect;
@@ -60,9 +62,46 @@ describe("CollisionHelper class", function () {
 
             })
         })
+        describe("Point/Triangle Collisions", function () {
+            let triangle = new TriangleCollider();
+            it("Points collide with Triangles they are in.", function () {
+                expect(true).to.be(false)
+
+            })
+            it("Points don't collide with Triangles they are not in.", function () {
+                expect(true).to.be(false)
+
+            })
+        })
+        describe("Point/Convex Collisions", function () {
+            let convex = new ConvexCollider();
+            it("Points collide with Convex Colliders they are in.", function () {
+                
+
+            })
+            it("Points don't collide with Convex Colliders they are not in.", function () {
+                expect(true).to.be(false)
+
+
+            })
+        })
     })
     describe("Circle Collisions", function () {
         let circle = new CircleCollider()
+        describe("Circle/Circle Collisions", function () {
+            let circle2 = new CircleCollider()
+            it("Circles collide with Circles they touch", function () {
+                circle.radius = 1;
+                circle2.radius = 2
+                
+                all(0,0,circle, 0,0, circle2, true)
+                
+            })
+            it("Circles don't collide with Circles they don't touch", function () {
+                all(2,0,circle, -1,0, circle2, false)
+
+            })
+        })
         describe("Circle/AABB Collisions", function () {
             let aabb = new AABBCollider()
             it("Circles collide with AABBs they touch", function () {
@@ -77,6 +116,94 @@ describe("CollisionHelper class", function () {
 
             })
         })
+        describe("Circle/Triangle Collisions", function () {
+            let triangle = new TriangleCollider();
+            it("Circles collide with Triangles they are in.", function () {
+                expect(true).to.be(false)
+
+            })
+            it("Circles don't collide with Triangles they are not in.", function () {
+                expect(true).to.be(false)
+
+            })
+        })
+        describe("Circle/Convex Collisions", function () {
+            let convex = new ConvexCollider();
+            it("Circles collide with Convex Colliders they are in.", function () {
+                
+
+            })
+            it("Circles don't collide with Convex Colliders they are not in.", function () {
+                expect(true).to.be(false)
+
+
+            })
+        })
     })
+    describe("AABB Collisions", function(){
+        describe("Circle/AABB Collisions", function () {
+            let aabb = new AABBCollider()
+            it("AABBs collide with AABBs they touch", function () {
+               expect(true).to.be(false)
+                
+            })
+            it("AABBs don't collide with AABBs they don't touch", function () {
+               expect(true).to.be(false)
+
+            })
+        })
+        describe("AABB/Triangle Collisions", function () {
+            let triangle = new TriangleCollider();
+            it("AABBs collide with Triangles they are in.", function () {
+                expect(true).to.be(false)
+
+            })
+            it("AABBs don't collide with Triangles they are not in.", function () {
+                expect(true).to.be(false)
+
+            })
+        })
+        describe("AABB/Convex Collisions", function () {
+            let convex = new ConvexCollider();
+            it("AABBs collide with Convex Colliders they are in.", function () {
+                
+
+            })
+            it("AABBs don't collide with Convex Colliders they are not in.", function () {
+                expect(true).to.be(false)
+
+
+            })
+        })
+
+    })
+    describe("TRiangle Collisions", function(){
+        let tri = new TriangleCollider()
+        describe("Triangle/Triangle Collisions", function () {
+            let triangle = new TriangleCollider();
+            it("Triangles collide with Triangles they are in.", function () {
+                expect(true).to.be(false)
+
+            })
+            it("Triangles don't collide with Triangles they are not in.", function () {
+                expect(true).to.be(false)
+
+            })
+        })
+        describe("Triangles/Convex Collisions", function () {
+            let convex = new ConvexCollider();
+            it("Triangles collide with Convex Colliders they are in.", function () {
+                
+
+            })
+            it("Triangles don't collide with Convex Colliders they are not in.", function () {
+                expect(true).to.be(false)
+
+
+            })
+        })
+
+    })
+
 
 });
