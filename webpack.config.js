dab4c9c96dd55b126afbe01bca1bb36a1a25a72b
B Ricks <bricks@unomaha.edu>
2020-03-28 06:45:28 -0500

    Webpack works in development mode. #38

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // 1
+  entry: './src/game.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  // 2
+  output: {
+    path: __dirname + '/production',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // 3
+  devServer: {
+    contentBase: './production'
+  }
+};
