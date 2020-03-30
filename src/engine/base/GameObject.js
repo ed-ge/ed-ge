87b05cfbc00c47ea78831661f0bad624d5491af8
B. Ricks <bricks@unomaha.edu>
2020-03-30 12:50:16 -0500

    Add hasComponent

--- src/engine/base/GameObject.js
+++ src/engine/base/GameObject.js
@@ -118,6 +118,19 @@ class GameObject extends NameableParent {
             throw "Error, couldn't find type " + type;
         }
     }
+    hasComponent(type) {
+        if (typeof(type) === 'string' || type instanceof String) {
+            //The user passed us a string, not a type
+            //https://stackoverflow.com/a/7772724/10047920
+            let component = this.components.find(i => i.constructor.name === type);
+            if (component) return true;
+            return false;
+        } else {
+            let component = this.components.find(i => i instanceof type);
+            if (component) return true;
+            return false;
+        }
+    }
     recursiveCall(functionName) {
         for (let i = 0; i < this.components.length; i++) {
             let component = this.components[i];
