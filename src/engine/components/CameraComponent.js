15ae0d78180ac744f0fa4f836ff503af72cc5d26
B. Ricks <bricks@unomaha.edu>
2020-03-30 13:00:37 -0500

    Add stub for Camera Component

---
+++ src/engine/components/CameraComponent.js
@@ -0,0 +1,20 @@
+import Base from "../Base.js"
+
+/**
+ * A component that indicates the containing game object is a virtual camera.
+ * This component also stores the default background color for the scene.
+ * There should be exactly one game object with exactly one instance
+ * of this class in each scene.
+ */
+
+class CameraComponent extends Base.Component {
+
+    constructor() {
+        super();
+       
+
+    }
+   
+}
+
+export default CameraComponent;
