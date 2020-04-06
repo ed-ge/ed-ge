47064b7ca3134b53210c796576151886aa1b6b6b
B. Ricks <bricks@unomaha.edu>
2020-04-06 12:51:23 -0500

    Add camera to the engine

---
+++ src/engine/base/Camera.js
@@ -0,0 +1,10 @@
+import GameObject from "./GameObject.js"
+
+class Camera extends GameObject{
+    constructor(){
+        this.backgroundColor = "white";
+        super();
+    }
+}
+
+export default Camera;
