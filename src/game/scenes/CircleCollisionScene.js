630d8da9c0ee2b05bb43b94856e550c78d2ee496
B Ricks <bricks@unomaha.edu>
2020-04-10 05:28:43 -0500

    Use new syntax in scene. #45

--- src/game/scenes/CircleCollisionScene.js
+++ src/game/scenes/CircleCollisionScene.js
@@ -7,35 +7,20 @@ export default {
       def:"CollisionCircle, 50, 50, 2, 1, CollisionCircle",
     },
     {
-      name: "Rectangle",
-      location: { x: 150, y: 50 },
-      rotation:.1,
-      scale:{x:2,y:.5},
-      type: "Rectangle"
+      def:"Rectangle, 150, 50, 2, .5, Rectangle"
     },
     {
-      name: "TestCollisionCircle",
-      location: { x: 200, y: 200 },
-      type: "TestCollisionCircle"
+      def:"TestCollisionCircle, 200, 200, TestCollisionCircle"
     },
     {
-      name: "CollisionCircle",
-      location: { x: 250, y: 50 },
-      scale:{x:1, y:2},
-      type: "CollisionCircle"
+      def:"CollisionCircle, 150, 50, 1, 2, CollisionCircle"
     },
     {
-      name: "Rectangle",
-      location: { x: 350, y: 50 },
-      type: "Rectangle"
+      def:"Rectangle, 350, 50, Rectangle"
     },
     {
-      name: "Main Controller",
-      location: { x: 100, y: 100 },
-      type: "EmptyGameObject",
-      children: [],
-      components: ["BackToStartSceneBehavior"]
+      def:"Main Controller, 100, 100, EmptyGameObject",
+      components:["BackToStartSceneBehavior"]
     }
-
   ]
 }
