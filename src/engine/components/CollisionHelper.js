09c315c4f3a6966898643262274f2ec0335fb5de
B Ricks <bricks@unomaha.edu>
2020-05-16 07:53:20 -0500

    Clean up html files

--- src/engine/components/CollisionHelper.js
+++ src/engine/components/CollisionHelper.js
@@ -5,81 +5,97 @@ import Point from "../base/Point.js"
 import Line from "../base/Line.js"
 import TriangleCollider from "./TriangleCollider.js"
 
-const CollisionHelper ={
+const CollisionHelper = {
 
-     inCollision(one, two) {
+    inCollision(one, two) {
         if (one.collider instanceof CircleCollider && two.collider instanceof PointCollider) {
-            let distance = one.gameObject.worldLocation.distance(two.gameObject.location);
-
-            if (distance < one.collider.radius * one.gameObject.scaleX)
-                return true; 
-            return false;
+            return this.inCollisionCirclePoint(one, two);
         } else if (one.collider instanceof PointCollider && two.collider instanceof CircleCollider) {
+            /** Flip */
             return this.inCollision(two, one);
         } else if (one.collider instanceof AABBCollider && two.collider instanceof PointCollider) {
-            //console.log("Testing AABB")
-            let diff = one.gameObject.location.diff(two.gameObject.location);
-            return Math.abs(diff.x) < one.collider.width / 2 && Math.abs(diff.y) < one.collider.height / 2;
-
+            return this.inCollisionAABBPoint(one, two);
         } else if (one.collider instanceof PointCollider && two.collider instanceof AABBCollider) {
+            /** Flip */
             return this.inCollision(two, one);
         } else if (one.collider instanceof CircleCollider && two.collider instanceof CircleCollider) {
-            let distance = one.gameObject.location.distance(two.gameObject.location);
-
-            if (distance < +one.collider.radius + +two.collider.radius)
-                return true;
-            return false;
-        } else if(one.collider instanceof AABBCollider && two.collider instanceof CircleCollider)
-        {
-          let cx = two.gameObject.x;
-          let cy = two.gameObject.y;
-          let rx = one.gameObject.x - one.collider.width/2;
-          let ry = one.gameObject.y - one.collider.height/2;
-          let rw = +one.collider.width;
-          let rh = +one.collider.height;
-
-          let tx = cx;
-          let ty = cy;
-
-          if(cx < rx) tx = rx;
-          else if(cx > rx + rw) tx = rx + rw;
-          if(cy < ry) ty = ry;
-          else if(cy > ry + rh) ty = ry + rh;
-
-          let diffX = (tx - cx)
-          let diffY = (ty - cy)
-          let distance = Math.sqrt(diffX*diffX+ diffY * diffY);
-          if(distance < +two.collider.radius)
-            return true;
-          return false;
-
-
+            return this.inCollisionCirclePoint(one, two);
+        } else if (one.collider instanceof AABBCollider && two.collider instanceof CircleCollider) {
+            return this.inCollisionAABBCircle(one, two);
         }
-        else if(one.collider instanceof CircleCollider && two.collider instanceof AABBCollider)
-        {
-          return this.inCollision(two, one);
+        else if (one.collider instanceof CircleCollider && two.collider instanceof AABBCollider) {
+            /** Flip */
+            return this.inCollision(two, one);
         }
-          else if (one.collider instanceof TriangleCollider && two.collider instanceof PointCollider) {
-            let pointA = new Point(+one.collider.pointAX + one.gameObject.x, +one.collider.pointAY + one.gameObject.y);
-            let pointB = new Point(+one.collider.pointBX + one.gameObject.x, +one.collider.pointBY + one.gameObject.y);
-            let pointC = new Point(+one.collider.pointCX + one.gameObject.x, +one.collider.pointCY + one.gameObject.y);
-
-            let lineOne = new Line(pointA, pointB);
-            let lineTwo = new Line(pointB, pointC);
-            let lineThree = new Line(pointC, pointA);
-
-            let distanceOne = lineOne.distance(two.gameObject.location)
-            let distanceTwo = lineTwo.distance(two.gameObject.location)
-            let distanceThree = lineThree.distance(two.gameObject.location)
-
-            return (distanceOne > 0 && distanceTwo > 0 && distanceThree > 0)
-            
+        else if (one.collider instanceof TriangleCollider && two.collider instanceof PointCollider) {
+            return this.inCollisionTrianglePoint(one, two);
         }
         else if (one.collider instanceof PointCollider && two.collider instanceof TriangleCollider) {
+            /**Flip */
             return this.inCollision(two, one);
         }
 
+    },
+    inCollisionCirclePoint(circle, point) {
+        let distance = circle.gameObject.worldLocation.distance(point.gameObject.location);
+
+        if (distance < circle.collider.radius * circle.gameObject.scaleX)
+            return true;
+        return false;
+    },
+    inCollisionAABBPoint(AABB, point) {
+        let diff = AABB.gameObject.location.diff(point.gameObject.location);
+        return Math.abs(diff.x) < AABB.collider.width / 2 && Math.abs(diff.y) < AABB.collider.height / 2;
+
+    },
+    inCollisionCircleCircle(circle1, circle2) {
+        let distance = circle1.gameObject.location.distance(circle2.gameObject.location);
+
+        if (distance < +circle1.collider.radius + +circle2.collider.radius)
+            return true;
+        return false;
+    },
+    inCollisionAABBCircle(AABB, Circle) {
+        let cx = Circle.gameObject.x;
+        let cy = Circle.gameObject.y;
+        let rx = AABB.gameObject.x - AABB.collider.width / 2;
+        let ry = AABB.gameObject.y - AABB.collider.height / 2;
+        let rw = +AABB.collider.width;
+        let rh = +AABB.collider.height;
+
+        let tx = cx;
+        let ty = cy;
+
+        if (cx < rx) tx = rx;
+        else if (cx > rx + rw) tx = rx + rw;
+        if (cy < ry) ty = ry;
+        else if (cy > ry + rh) ty = ry + rh;
+
+        let diffX = (tx - cx)
+        let diffY = (ty - cy)
+        let distance = Math.sqrt(diffX * diffX + diffY * diffY);
+        if (distance < +Circle.collider.radius)
+            return true;
+        return false;
+    },
+    inCollisionTrianglePoint(triangle, point) {
+        let pointA = new Point(+triangle.collider.pointAX + triangle.gameObject.x, +triangle.collider.pointAY + triangle.gameObject.y);
+        let pointB = new Point(+triangle.collider.pointBX + triangle.gameObject.x, +triangle.collider.pointBY + triangle.gameObject.y);
+        let pointC = new Point(+triangle.collider.pointCX + triangle.gameObject.x, +triangle.collider.pointCY + triangle.gameObject.y);
+
+        let lineOne = new Line(pointA, pointB);
+        let lineTwo = new Line(pointB, pointC);
+        let lineThree = new Line(pointC, pointA);
+
+        let distanceOne = lineOne.distance(point.gameObject.location)
+        let distanceTwo = lineTwo.distance(point.gameObject.location)
+        let distanceThree = lineThree.distance(point.gameObject.location)
+
+        return (distanceOne > 0 && distanceTwo > 0 && distanceThree > 0)
     }
+
+
+
 
 }
 
