e07cee002a14a9ac281bce56062582198eb36b69
bricksphd <bricks@unomaha.edu>
2020-05-17 11:43:26 +0000

    Deploying to build from  069394143a5ba9b2b5cbfe91e85f51e0777edf32

---
+++ test/mocha/engine/base/SceneManager.spec.js
@@ -0,0 +1,20 @@
+import chai from "chai";
+
+import Base from "../../../../src/engine/Base.js"
+import Scenes from "../../../game/Scenes.js"
+import GameObjects from "../../../game/GameObjects.js"
+import GameBehaviors from "../../../game/GameBehaviors.js"
+
+describe("Engine", function () {
+  describe("Base", function () {
+    describe("SceneManager", function () {
+      describe("Get Current Scene", function () {
+        it("Should return the current scene", function () {
+          Base.main(GameObjects, GameBehaviors, Scenes, { runUpdate: false, runDraw: false, startScene: 'RoomScene' });
+          let currentScene = Base.SceneManager.currentScene;
+          chai.expect(currentScene).to.equal(Base.SceneManager.scenes.find(i => i.name == 'RoomScene'));
+        })
+      })
+    })
+  })
+})
