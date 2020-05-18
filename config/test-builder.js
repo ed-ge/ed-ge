933b609bfe452778534022d16c4324b19c29074a
B Ricks <bricks@unomaha.edu>
2020-05-18 05:54:27 -0500

    Add script to fleshout test files

--- config/test-builder.js
+++ config/test-builder.js
@@ -8,7 +8,32 @@ let testRoot = "./test/mocha";
 
 recursive(srcPath, function (err, files) {
   // `files` is an array of file paths
-  files.forEach(f=>console.log(path.join(testRoot, f.substr(srcPath.length))));
+  files.forEach(f=>
+    {
+      let index = f.lastIndexOf("\\");
+      let className = f.substring(index + 1);
+      let filePath =path.join(testRoot, f.substr(srcPath.length));
+      filePath = filePath.replace(".js", ".spec.js")
+      if(!fs.existsSync("./" + filePath)){
+        console.log(filePath);
+        let fileContents =
+`
+import chai from "chai";
+
+import Base from "../../../../src/Base.js"
+import Scenes from "../../../game/Scenes.js"
+import GameObjects from "../../../game/GameObjects.js"
+import GameBehaviors from "../../../game/GameBehaviors.js"
+
+describe("Base", function () {
+  describe("${className}", function () {
+
+  });
+});
+`
+fs.writeFileSync(filePath, fileContents);
+      }
+    });
   
 });
 
