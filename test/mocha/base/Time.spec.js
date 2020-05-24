c3ad8e8de019317a71ce4fe0bc972f49438cb1f7
bricksphd <bricksphd@users.noreply.github.com>
2020-05-24 12:33:31 +0000

    Deploying to build from  21c0f727f4e320dfdcee7e8b1d3e18f0d00ca18d

--- test/mocha/base/Time.spec.js
+++ test/mocha/base/Time.spec.js
@@ -1,5 +1,6 @@
 
 import chai from "chai";
+const expect = chai.expect;
 
 import Base from "../../../src/Base.js"
 import Scenes from "../../game/Scenes.js"
@@ -8,6 +9,13 @@ import GameBehaviors from "../../game/GameBehaviors.js"
 
 describe("Base", function () {
   describe("Time.js", function () {
-
+    describe("Time constructor", function(){
+     it("Creates a member variable set to 0", function(){
+       let time = new Base.Time();
+       expect(time).to.not.be.undefined;
+       expect(time).to.not.be.null;
+       expect(time.deltaTime).to.equal(0);
+     })
+    })
   });
 });
