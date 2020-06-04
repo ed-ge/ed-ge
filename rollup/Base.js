138c535b05dea3909c154ddab92cae8d41cb331b
bricksphd <bricks@unomaha.edu>
2020-06-04 19:29:25 +0000

    Deploying to build from  967218122ddc68aca74eddee87f2242dbd913c4e

--- rollup/Base.js
+++ rollup/Base.js
@@ -564,6 +564,7 @@ const Input = {
 
 
   swapUpDownArrays() {
+    if(arguments.length != 0) throw new Error("swapUpDownArrays takes no arguments.")
     this.frameDown = this.down;
     this.frameUp = this.up;
     this.down = [];
@@ -605,16 +606,19 @@ const Input = {
 
   //Did the key come up this frame?
   getKeyUp(key) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.frameUp[key];
   },
 
   //Did the key go down the frame? [Remember, the OS may make it look like key repeated when they did not]
   getKeyDown(key) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.frameDown[key];
   },
 
   //Is the key pressed? Down (true) Up (false)
   getKey(key) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.keys[key];
   },
 
@@ -625,52 +629,64 @@ const Input = {
 
   //Did the mouse button come up this frame?
   getMouseButtonUp(button) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.frameMouseButtonsUp[button];
   },
 
   //Did the mouse button go down this frame?
   getMouseButtonDown(button) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.frameMouseButtonsDown[button];
   },
 
   //Is the mouse button pressed? Down (true) Up (false)
   getMouseButton(button) {
+    if(arguments.length != 1) throw new Error("Function requires exactly one argument.")
     return this.mouseButtons[button];
   },
 
   //What is the current state of the scroll wheel?
   getMouseScrollWheel() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.frameScrollDelta;
   },
 
   //What is the mouse position?
   //We return the previous frame's position for consistency
   getMousePosition() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.frameMousePosition;
   },
   getMousePositionDelta() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return new Point(this.frameMousePosition.x - this.lastFrameMousePosition.x, this.frameMousePosition.y - this.lastFrameMousePosition.y);
   },
 
   //Touch API----------------------------------
   //
   getTouchesStartFull() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.frameTouchesStart;
   },
   getTouchesStart() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.frameTouchesStart.map(i => { return { x: i.clientX, y: i.clientY } });
   },
   getTouchesEndFull() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.frameTouchesEnd;
   },
   getTouchesEnd() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     if(this.frameTouchesEnd.length == 0) return [];
     return this.frameTouchesEnd.map(i => { return { x: i.clientX, y: i.clientY } });
   },
   getTouchesFull() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     return this.touches;
   },
   getTouches() {
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     //If we have ending touches, merge those in here
     let toReturn = this.touches.map(i => { return { x: i.clientX, y: i.clientY } });
     toReturn.push(...this.frameTouchesEnd.map(i=>{return {x:i.clientX,y:i.clientY}}));
@@ -680,6 +696,7 @@ const Input = {
   //   return this.frameTouchPositions.map(i => { return { x: i.clientX, y: i.clientY } });
   // },
   getTouchMove(){    
+    if(arguments.length != 0) throw new Error("Function does not accept arguments.")
     if(this.frameTouchPositions.length == 0 || this.lastFrameTouchPositions.length == 0) return [{x:0,y:0}];
     let frames = this.frameTouchPositions.map(i => { return { x: i.clientX, y: i.clientY } });
     let currents = this.lastFrameTouchPositions.map(i => { return { x: i.clientX, y: i.clientY } });
