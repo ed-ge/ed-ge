5c7408ac5131a2d68c0b62338c9ed754ddef811a
bricksphd <bricks@unomaha.edu>
2020-06-18 12:02:17 +0000

    Deploying to build from  4ab3ecb11de674cdc063857e50d9b438d27ec767

--- test/mocha/base/Scene.spec.js
+++ test/mocha/base/Scene.spec.js
@@ -1,15 +1,36 @@
 
 import chai from "chai";
+const expect = chai.expect;
 
 import Base from "../../../src/Base.js"
 import Scenes from "../../game/Scenes.js"
 import GameObjects from "../../game/GameObjects.js"
 import GameBehaviors from "../../game/GameBehaviors.js"
+import Scene from "../../../src/base/Scene.js";
 
 describe("Base", function () {
   describe("Scene.js", function () {
     describe("constructor", function(){
-      it("Throws an error on mismatched arguments", function () {
+      // it("Throws an error on mismatched arguments", function () {
+      //   expect(()=>new Scene()).to.throw();
+      //   expect(()=>new Scene(Scenes.allScenes[0])).to.throw();
+      //   expect(()=>new Scene(Scenes.allScenes[0], Base.Prefabs)).to.throw();
+      //   expect(()=>new Scene(Scenes.allScenes[0], Base.Prefabs, GameBehaviors)).to.throw();
+      //   expect(()=>new Scene(Scenes.allScenes[0], Base.Prefabs, GameBehaviors, Base.Components, 1)).to.throw();
+      // })
+      it("Creates a new scene", function(){
+        // Base.main(GameObjects, GameBehaviors, Scenes, { runUpdate: false, runDraw: false, startScene: 'RoomScene' });
+        let scene = new Scene(Scenes.allScenes[0], Base.Prefabs, GameBehaviors, Base.Components);
+        expect(scene.name).to.equal("SceneOne");
+        expect(scene.children).to.be.an('array').that.is.empty;
+        expect(scene.objects).to.equal(Scenes.allScenes[0].objects);
+        expect(scene.prefabs).to.equal(Base.Prefabs);
+        expect(scene.behaviors).to.equal(GameBehaviors);
+        expect(scene.components).to.equal(Base.Components);
+        expect(scene.layers).to.be.an('array').of.length(3);
+        expect(scene.layers).to.be.an('array').of.length(3).that.includes('background');
+        expect(scene.layers).to.be.an('array').of.length(3).that.includes(null);
+        expect(scene.layers).to.be.an('array').of.length(3).that.includes('foreground');
       })
     })
     describe("boot function", function(){
