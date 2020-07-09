b0a8bb59c95a41750bf511589bd84461ef0ee5c1
bricksphd <bricks@unomaha.edu>
2020-07-09 11:00:45 +0000

    Deploying to build from  ce89bf87846b9bbbfe072114807b380e360b944f

--- test/browser-tests/card-game/CardComponent.js
+++ test/browser-tests/card-game/CardComponent.js
@@ -2,10 +2,13 @@ import Base from "../../../src/Base.js"
 export default class CardComponent extends Base.Behavior{
   start(){
     this.value = {};
-
+    
   }
+ 
+ 
   update(){
-   this.gameObject.getComponent("TextComponent").text = JSON.stringify(this.value.name);
+    this.gameObject.getComponent("TextComponent").text = JSON.stringify(this.value.name);
+    
   }
 
 }
