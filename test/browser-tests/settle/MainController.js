03a8c152d8591240e823ba2b2cac8e9793f0faa5
B Ricks <bricks@unomaha.edu>
2020-07-19 06:29:52 -0500

    Add  settle game

---
+++ test/browser-tests/settle/MainController.js
@@ -0,0 +1,10 @@
+import Base from "../../../src/Base.js"
+export default class MainController extends Base.Behavior {
+  start() {
+   
+    
+  }
+ 
+  
+}
+
---
+++ test/browser-tests/settle/index.js
@@ -0,0 +1,159 @@
+//Import any custom behaviors
+import Base from "../../../src/Base.js"
+import MainController from "./MainController.js"
+
+//Add custom behaviors to an object we pass to Main
+let GameBehaviors = {
+  MainController,
+}
+
+//Add objects to our scene that we pass to main
+let Scenes = {
+  startScene: "StartScene",
+  allScenes: [
+`
+StartScene
+
+BoardBase Empty
+0,0
+
+WheatStack WheatStack
+0,0 
+
+SheepStack SheepStack
+0,50 
+
+BrickStack BrickStack
+0,100 
+
+WoddStack WoodStack
+0,150
+
+StoneStack StoneStack
+0,200 
+
+
+MainCamera Camera
+200,200
+
+
+MainController Empty
+MainController
+`
+  ]
+}
+
+
+
+//Declare the prefabs we will pass to Main
+let Wheat =
+`
+Wheat Circle
+Draggable
+CircleComponent
+ fill=yellow
+ radius=20
+CircleCollider
+ radius=20
+`
+let Sheep =
+`
+Sheep Circle
+Draggable
+CircleComponent
+ fill=green
+ radius=20
+CircleCollider
+ radius=20
+`
+let Brick =
+`
+Brick Circle
+Draggable
+CircleComponent
+ fill=red
+ radius=20
+CircleCollider
+ radius=20
+`
+let Wood =
+`
+Wood Circle
+Draggable
+CircleComponent
+ fill=chocolate
+ radius=20
+CircleCollider
+ radius=20
+`
+let Stone =
+`
+Stone Circle
+Draggable
+CircleComponent
+ fill=gray
+ radius=20
+CircleCollider
+ radius=20
+`
+let WheatStack = 
+`
+WheatStack Circle
+Stack
+ spawn=Wheat
+CircleComponent
+ fill=yellow
+ radius=20
+CircleCollider
+ radius=20
+ `
+let SheepStack = 
+`
+SheepStack Circle
+Stack
+ spawn=Sheep
+CircleComponent
+ fill=green
+ radius=20
+CircleCollider
+ radius=20
+ `
+let BrickStack = 
+`
+BrickStack Circle
+Stack
+ spawn=Brick
+CircleComponent
+ fill=red
+ radius=20
+CircleCollider
+ radius=20
+ `
+let StoneStack = 
+`
+StoneStack Circle
+Stack
+ spawn=Stone
+CircleComponent
+ fill=gray
+ radius=20
+CircleCollider
+ radius=20
+ `
+let WoodStack = 
+`
+WoodStack Circle
+Stack
+ spawn=Wood
+CircleComponent
+ fill=chocolate
+ radius=20
+CircleCollider
+ radius=20
+ `
+
+
+//Place the prefabs in an object we will pass to Main
+let Prefabs = { Wheat, Sheep, Brick, Wood, Stone, WheatStack, SheepStack, BrickStack, WoodStack, StoneStack  };
+
+Base.Main = Base.main(Prefabs, GameBehaviors, Scenes);
