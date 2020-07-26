d15dd27e5cbb17b93861c3ec83fdde70a91d2f73
B Ricks <bricks@unomaha.edu>
2020-07-25 19:59:34 -0500

    Fixe regex grouping error

--- test/lexer/grammar.js
+++ test/lexer/grammar.js
@@ -5,7 +5,9 @@ function id(x) { return x[0]; }
 
 const lexer = moo.compile({
   wschar: {match:/[ \t\n\v\f]/, lineBreaks:true},
+  float: /^[+-]?\d+(?: \.\d+)?$/, //From https://stackoverflow.com/a/10256077/10047920
   word: /[a-zA-Z_][a-zA-_Z0-9]*/,
+  ',':',',
   });
 
 
@@ -31,9 +33,25 @@ var grammar = {
     Lexer: lexer,
     ParserRules: [
     {"name": "Grammar", "symbols": ["Object"], "postprocess": id},
-    {"name": "Object", "symbols": ["Name", "__", "Prefab"], "postprocess": (args)=>{return{name:args[0], prefab:args[2]}}},
+    {"name": "Object$ebnf$1$subexpression$1", "symbols": ["__", "Transforms"]},
+    {"name": "Object$ebnf$1", "symbols": ["Object$ebnf$1$subexpression$1"], "postprocess": id},
+    {"name": "Object$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Object", "symbols": ["Name", "__", "Prefab", "Object$ebnf$1"], "postprocess": (args)=>{return{name:args[0], prefab:args[2], transforms:args[3]}}},
     {"name": "Name", "symbols": ["Word"], "postprocess": id},
     {"name": "Prefab", "symbols": ["Word"], "postprocess": id},
+    {"name": "Transforms$ebnf$1$subexpression$1", "symbols": ["__", "SecondTransforms"]},
+    {"name": "Transforms$ebnf$1", "symbols": ["Transforms$ebnf$1$subexpression$1"], "postprocess": id},
+    {"name": "Transforms$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Transforms", "symbols": ["Translation", "Transforms$ebnf$1"], "postprocess": d=> {return {tronslate: d[0], scale:d[0].scale, rotation:d[0].rotation}}},
+    {"name": "SectondTransforms$ebnf$1$subexpression$1", "symbols": ["__", "Rotation"]},
+    {"name": "SectondTransforms$ebnf$1", "symbols": ["SectondTransforms$ebnf$1$subexpression$1"], "postprocess": id},
+    {"name": "SectondTransforms$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "SectondTransforms", "symbols": ["Scale", "SectondTransforms$ebnf$1"], "postprocess": d=> {return {scale: d[0], rotation:d[1]}}},
+    {"name": "Rotation", "symbols": ["Float"], "postprocess": id},
+    {"name": "Translation", "symbols": ["Point"], "postprocess": id},
+    {"name": "Scale", "symbols": ["Point"], "postprocess": id},
+    {"name": "Point", "symbols": ["Float", {"literal":","}, "Float"], "postprocess": d => { return {x:d[0],y:d[2]}}},
+    {"name": "Float", "symbols": [(lexer.has("float") ? {type: "float"} : float)], "postprocess": getValue},
     {"name": "Word", "symbols": [(lexer.has("word") ? {type: "word"} : word)], "postprocess": getValue},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
