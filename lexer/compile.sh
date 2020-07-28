dcc48c61aad06ab8d3370405e4f8d5b708f6bc77
B Ricks <bricks@unomaha.edu>
2020-07-28 04:55:03 -0500

    Pull CS6 conversion into a script

--- lexer/compile.sh
+++ lexer/compile.sh
@@ -1,6 +1,6 @@
-nearleyc ./lexer/lexer.ne -o ./lexer/grammar.js
+cd lexer
+nearleyc ./lexer.ne -o ./grammar.js
 # By default, nearleyc creates a commonjs script. We need to convert it to es6
-sed -i -e 1,3d ./lexer/grammar.js # Remove the first 3 lines
-truncate -s -$(tail -6 ./lexer/grammar.js | wc -c) ./lexer/grammar.js # remove the last 6 lines
-echo 'export default grammar' >> ./lexer/grammar.js # Append export default 
+./convertES6.sh ./grammar.js
+cd ..
 node ./test/lexer/parse.js
