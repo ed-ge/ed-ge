3c5c4f1ed80a872232b43ffaedc4d7e3d54eeab8
B Ricks <bricks@unomaha.edu>
2020-08-03 19:42:39 -0500

    Clean lexer folder

--- lexer/compileNoES6.sh
+++
@@ -1,4 +0,0 @@
-nearleyc ./lexer/objectLexer.ne -o ./lexer/objectCommonGrammar.js
-sed -i -e 's@import moo from "../lib/moo.js";@const moo=require("../lib/moo.js");@' ./lexer/objectCommonGrammar.js
-nearleyc ./lexer/sceneLexer.ne -o ./lexer/sceneCommonGrammar.js
-sed -i -e 's@import moo from "../lib/moo.js";@const moo=require("../lib/moo.js");@' ./lexer/sceneCommonGrammar.js
--- lexer/objectLexer.ne
+++ lexer/objectLexer.ne
@@ -1,6 +1,6 @@
 @{%
 //Allow use in node and in browser
-    import moo from "../lib/moo.js";
+    import moo from "../lib/lexer/moo.js";
 
 const lexer = moo.compile({
   componentLine :/=[ \t]*.+[ \t]*$/,
