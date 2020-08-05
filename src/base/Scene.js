4615ee5863a4c9f2ad246eb8e4417b5f7e653de9
bricksphd <bricks@unomaha.edu>
2020-08-05 10:05:45 +0000

    Deploying to build from  f334b6a3cc42f5549beb5b85f2bf67e7e78f2304

--- src/base/Scene.js
+++ src/base/Scene.js
@@ -48,8 +48,7 @@ class Scene extends NameableParent {
     this.layers = ["background", null, "foreground"];
 
     this.frameMouseOver = [];
-    this.frameCollisionOver = [];
-
+    
     this.lastCtx = null;
   }
 
@@ -126,12 +125,10 @@ class Scene extends NameableParent {
     }
   }
 
-  
-
   isInScreenSpace(gameObject) {
     if (arguments.length != 1 || !(gameObject instanceof Base.GameObject)) throw new Error("isInScreenSpace expects exactly one argument of type GameObject")
 
-    let canvases = this.children.filter(i => i.anyComponent("CanvasComponent"));
+    let canvases = Base.$$.children.filter(i => i.anyComponent("CanvasComponent"));
     if (canvases.length == 0) return false; // We don't have screen space
     for (let canvas of canvases) {
       if (canvas.isADescendant(gameObject)) {
@@ -139,6 +136,39 @@ class Scene extends NameableParent {
       }
     }
     return false;
+  }
+
+  
+
+    /**
+   * Get a flat list of all the collidable components in the scene
+   * @param {*} gameObject The root game object in the tree we are searching
+   * @param {*} collidableChildren The list we are modifying
+   * @param {*} type The type a game object needs in order to be considered collidable
+   */
+  getCollidable(gameObject, collidableChildren, type) {
+    if (arguments.length != 3 ||
+      !(typeof gameObject == 'object') ||
+      !(Array.isArray(collidableChildren)) ||
+      !(typeof type == 'function')) throw new Error("getCollidable expects exactly three arguments of type GameObject, array, and type")
+
+
+    if (gameObject.getComponent) {
+      try {
+        let collidableComponent = gameObject.getComponent(type);
+        if (collidableComponent) {
+          collidableChildren.push({ collider: collidableComponent, gameObject });
+        }
+      } catch (e) {
+        //no-op
+      }
+    }
+
+    for (let i = 0; i < gameObject.children.length; i++) {
+      let child = gameObject.children[i];
+
+      this.getCollidable(child, collidableChildren, type);
+    }
   }
 
   /**
@@ -158,56 +188,9 @@ class Scene extends NameableParent {
      * Now run the simulators
      */
 
-    //
-    //First we do collisions
-    //
-
-    //Add collision behavior
     let collidableChildren = [];
     this.getCollidable(this, collidableChildren, collidableType);
 
-    for (let i = 0; i < collidableChildren.length; i++) {
-      let gameObjectOne = collidableChildren[i].gameObject;
-      let isInScreenSpaceOne = this.isInScreenSpace(gameObjectOne);
-      for (let j = i + 1; j < collidableChildren.length; j++) {
-        let gameObjectTwo = collidableChildren[j].gameObject;
-        let isInScreenSpaceTwo = this.isInScreenSpace(gameObjectTwo);
-        if (isInScreenSpaceOne != isInScreenSpaceTwo) break;
-        let collisionPair = { one: collidableChildren[i], two: collidableChildren[j] };
-        if (collisionHelper.inCollision(collidableChildren[i], collidableChildren[j])) {
-          gameObjectOne.components.forEach(x => {
-            if (x.onCollisionStay)
-              x.onCollisionStay(collidableChildren[j])
-          })
-          gameObjectTwo.components.forEach(x => {
-            if (x.onCollisionStay)
-              x.onCollisionStay(collidableChildren[i])
-
-          })
-          if (this.frameCollisionOver.findIndex(x=>this.collisionPairMatch(x,collisionPair))==-1) {
-            this.frameCollisionOver.push(collisionPair);
-            gameObjectOne.components.filter(x=>x.onCollisionEnter).forEach(x => {
-              x.onCollisionEnter(collidableChildren[j])
-            })
-            gameObjectTwo.components.filter(x=>x.onCollisionEnter).forEach(x => {
-              x.onCollisionEnter(collidableChildren[i])
-            })
-          }
-        }
-        else{
-          if(this.frameCollisionOver.findIndex(x=>this.collisionPairMatch(x,collisionPair))!=-1)
-          {
-            this.frameCollisionOver = this.frameCollisionOver.filter(x=>!this.collisionPairMatch(x, collisionPair));
-            gameObjectOne.components.filter(x=>x.onCollisionExit).forEach(x => {
-              x.onCollisionExit(collidableChildren[j])
-            })
-            gameObjectTwo.components.filter(x=>x.onCollisionExit).forEach(x => {
-              x.onCollisionExit(collidableChildren[i])
-            })
-          }
-        }
-      }
-    }
 
     //
     //Now go through and see if the point represented by the mouse collides with any of the colliders
@@ -358,43 +341,9 @@ class Scene extends NameableParent {
       }
     }
   }
-  collisionPairMatch(one, two){
-    return one.one.gameObject == two.one.gameObject &&
-      one.two.gameObject == two.two.gameObject &&
-      one.one.collider == two.one.collider &&
-      one.two.collider == two.two.collider;
-  }
+  
 
-  /**
-   * Get a flat list of all the collidable components in the scene
-   * @param {*} gameObject The root game object in the tree we are searching
-   * @param {*} collidableChildren The list we are modifying
-   * @param {*} type The type a game object needs in order to be considered collidable
-   */
-  getCollidable(gameObject, collidableChildren, type) {
-    if (arguments.length != 3 ||
-      !(typeof gameObject == 'object') ||
-      !(Array.isArray(collidableChildren)) ||
-      !(typeof type == 'function')) throw new Error("getCollidable expects exactly three arguments of type GameObject, array, and type")
-
-
-    if (gameObject.getComponent) {
-      try {
-        let collidableComponent = gameObject.getComponent(type);
-        if (collidableComponent) {
-          collidableChildren.push({ collider: collidableComponent, gameObject });
-        }
-      } catch (e) {
-        //no-op
-      }
-    }
-
-    for (let i = 0; i < gameObject.children.length; i++) {
-      let child = gameObject.children[i];
-
-      this.getCollidable(child, collidableChildren, type);
-    }
-  }
+  
 
   /**
    * Convert the point in screen space to world space
