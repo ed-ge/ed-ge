e1048d15be3f1dd49a4e3bd48591bac2069c6bde
B Ricks <bricks@unomaha.edu>
2020-08-16 15:59:41 -0500

    Fix drag out issue

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,6 +1,7 @@
 import Behavior from "../base/Behavior.js"
 import SceneManager from "../base/SceneManager.js"
 import Serializer from "../base/Serializer.js"
+import Base from "../Base.js"
 
 
 export default class Stack extends Behavior {
@@ -28,14 +29,14 @@ export default class Stack extends Behavior {
   }
   onMouseDown() {
     this.mouseDown = true;
-    let spawned = SceneManager.Base.Serializer.instantiate(SceneManager.Prefabs[this.spawn], this.$go.parent, this.$go.worldLocation);
+    let spawned = SceneManager.Base.Serializer.instantiate(SceneManager.Prefabs[this.spawn], Base._cs, this.$go.worldLocation);
     spawned.$("Draggable").mouseDown = true;
   }
   // onMouseUp() {
   //   this.mouseDown = false;
   // }
   onTouchStart(){
-    let spawned = SceneManager.Base.Serializer.instantiate(SceneManager.Prefabs[this.spawn], this.$go.parent, this.$go.worldLocation);
+    let spawned = SceneManager.Base.Serializer.instantiate(SceneManager.Prefabs[this.spawn], Base._cs, this.$go.worldLocation);
     spawned.$("Draggable").touchDown = true;
   }
   
--- src/components/TextComponent.js
+++ src/components/TextComponent.js
@@ -6,12 +6,20 @@ class TextComponent extends Component {
         this.text = "[Blank]";
         this.font = "10pt Sans";
         this.fill = "black";
+        this.centered = false;
     }
     draw(ctx) {
         ctx.save();
         ctx.fillStyle = this.fill;
         ctx.font = this.font;
-        ctx.fillText(this.text, 0, 0);
+        if (!this.centered)
+            ctx.fillText(this.text, 0, 0);
+        else{
+            let metrics = ctx.measureText(this.text);
+            let fontHeight = metrics.actualBoundingBoxAscent;
+            
+            ctx.fillText(this.text, -metrics.width/2, fontHeight/2)
+        }
         ctx.restore();
     }
     update() {
