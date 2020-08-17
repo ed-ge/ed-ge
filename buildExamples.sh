4aa405fecd9f8095b94a308ca07d1b54e9795e7d
bricksphd <bricks@unomaha.edu>
2020-08-17 10:48:10 +0000

    Deploying to build from  c62d4329f97abea83d2fa5eb00d22344968a1eb9

--- buildExamples.sh
+++ buildExamples.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cp -r ./test/examples/* ./examples
 #<script src="https://cdn.jsdelivr.net/gh/ed-ge/ed-ge@latest/free/Base.js"></script>
 find ./examples/* -name "*html" -type f | xargs sed -i -e '/jsdelivr/ a <script src="https://cdn.jsdelivr.net/npm/@ed-ge/ed-ge@latest/free/Base.js"></script>'
